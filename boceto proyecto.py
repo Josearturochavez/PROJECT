038853974484e2fbf30b8ded842ca0158cac85e2
josearturochavez <chavez.yzquierdo.jose.21@gmail.com>
2024-06-24 21:32:53 -0600

    ejercico 1 terminado

--- boceto proyecto.py
+++ boceto proyecto.py
@@ -1,24 +1,29 @@
-"""
-habra un profesor y un asistente
-pedir el nombre y edad de los compañeros
-ordenar a los compañeros por edad
-"""
-#obteniendo el numero de alumnos que asistieron
-print("ingresa el numero de compañeros que asistieron")
-num_companeros = int(input())
+def obtener_suplentes():
+    #obteniendo el numero de alumnos que asistieron
+    print("ingresa el numero de compañeros que asistieron")
+    num_companeros = int(input())
+    
+    #dando instrucciones para ingresar datos al usuario
+    print("ingresa el nombre del compañero y su edad separados por una linea, repite el proceso con todos los compañeros")
 
-#dando instrucciones para ingresar datos al usuario
-print("ingresa el nombre del compañero y su edad separados por una linea, repite el proceso con todos los compañeros")
+    #declarando la lista de compañeros
+    lista_companeros = []
 
-#declarando la lista de compañeros
-lista_companeros = []
+    #introduccion de datos por cada compañero
+    for i in range(num_companeros):
+        nombre = input()
+        edad = int(input())
+        nuevo_compañero = (nombre,edad)
+        lista_companeros.append(nuevo_compañero)
 
-#introduccion de datos por cada compañero
-for i in range(num_companeros):
-    nombre = input()
-    edad = int(input())
-    nuevo_compañero = (nombre,edad)
-    lista_companeros.append(nuevo_compañero)
+    #se ordenan los compañeros por edad
+    lista_companeros.sort(key=lambda x: x[1])
+    
+    #asigna profesor y asistente y da sus edades
+    profesor,profesor_edad = lista_companeros[-1]
+    asistente,asistente_edad = lista_companeros[0]
 
-print(lista_companeros)
+    return profesor,profesor_edad,asistente,asistente_edad
 
+profesor,profesor_edad,asistente,asistente_edad =  obtener_suplentes()
+print (f"el profesor es {profesor} con {profesor_edad} años y el asistente es {asistente} con {asistente_edad} años")
